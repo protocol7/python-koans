7d8b1f97ab4f3c09f0470e2b24a4740044691ac4
Niklas Gustavsson <niklas@protocol7.com>
2011-07-26 15:49:19 +0200

    Completed final koan

--- python 2/koans/about_proxy_object_project.py
+++ python 2/koans/about_proxy_object_project.py
@@ -21,12 +21,38 @@ from runner.koan import *
 class Proxy(object):
     def __init__(self, target_object):
         # WRITE CODE HERE
+
+        self._messages = []
         
         #initialize '_obj' attribute last. Trust me on this!
         self._obj = target_object
 
-    # WRITE CODE HERE                                   
+    def __getattr__(self, attr_name):
+      self._messages.append(attr_name)
+      return self._obj.__getattribute__(attr_name)
+   
+    def __setattr__(self, attr_name, value):
+      names = ["_obj", "_messages", "messages", "was_called", "number_of_times_called"]
+      if attr_name in names:
+        return object.__setattr__(self, attr_name, value)
+      else:
+        self._messages.append(attr_name + "=")
+        self._obj.__setattr__(attr_name, value)                
 
+    def messages(self):
+      return self._messages
+
+    def was_called(self, m):
+      return m in self._messages
+
+    def number_of_times_called(self, m):
+      count = 0
+      for message in self._messages:
+        if message == m:
+          count += 1
+
+      return count
+      
 # The proxy object should pass the following Koan:
 #
 class AboutProxyObjectProject(Koan):
